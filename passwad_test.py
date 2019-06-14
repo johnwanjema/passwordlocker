33a4fd0afafd200cd8a906b9828caf2aa2bf362f
johnwanjema <jonwanjema@gmail.com>
2019-06-14 11:49:16 +0300

    added test to save multiple users and credentilas using tear dwn

--- passwad_test.py
+++ passwad_test.py
@@ -1,6 +1,6 @@
 import unittest # Importing the unittest module
 from passwad import user # Importing the user class
-from passwad import credentials # Importing the credentilas class
+from passwad import credentials # Importing the credentials class
 
 
 class Testuser(unittest.TestCase):
@@ -20,6 +20,12 @@ class Testuser(unittest.TestCase):
         '''
         self.new_user= user("James","1234")  # create user  object
 
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            user.user_list = []
+
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
@@ -36,6 +42,16 @@ class Testuser(unittest.TestCase):
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(user.user_list),1)
+
+    def test_save_multiple_users(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_user.save_user()
+            test_user = user("john","4321") # new user
+            test_user.save_user()
+            self.assertEqual(len(user.user_list),2)
        
 
 class Testcredentials(unittest.TestCase):
@@ -53,7 +69,13 @@ class Testcredentials(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_credentials= credentials("twitter","1234")  # create user  object
+        self.new_credentials= credentials("twitter","1234")  # create credentila  object
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            credentials.credentials_list = []
 
     def test_init(self):
         '''
@@ -63,13 +85,23 @@ class Testcredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.account_name, "twitter")
         self.assertEqual(self.new_credentials.password, "1234")
 
-    def test_save_credentilas(self):
+    def test_save_credentials(self):
         '''
         test_save_contact test case to test if the contact object is saved into
          the contact list
         '''
         self.new_credentials.save_credentials() # saving the new user
         self.assertEqual(len(credentials.credentials_list),1)
+
+    def test_save_multiple_credentials(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple contact
+            objects to our contact_list
+            '''
+            self.new_credentials.save_credentials()
+            test_credentials = credentials("john","4321") # new credential
+            test_credentials.save_credentials()
+            self.assertEqual(len(credentials.credentials_list),2)
 
 
 if __name__ == '__main__':
