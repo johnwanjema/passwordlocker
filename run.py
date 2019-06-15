10e5d8bec4b5d1b69a686e71f66daf7cb46c4f60
johnwanjema <jonwanjema@gmail.com>
2019-06-15 12:22:56 +0300

    added functions tocreta and save users and accounts

--- run.py
+++ run.py
@@ -2,7 +2,35 @@
 from passwad import user # Importing the user class
 from passwad import account # Importing the account class
 
+#user functions
+def create_user(user_name,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = user(user_name,password)
+    return new_user
 
+def save_user(user):
+    '''
+    Function to save users
+    '''
+    user.save_user()
+
+
+#account functios
+def create_account(account_name,password):
+    '''
+    Function to create a new account
+    '''
+    new_account = user(account_name,password)
+    return new_account
+
+
+def save_account(account):
+    '''
+    Function to save accounts
+    '''
+    account.save_account()
 
 
 
